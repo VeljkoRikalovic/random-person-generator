d365182f78a6a2829e16e2893bf2f03534ef844c
Veljko <velja001@yahoo.com>
2023-02-24 18:15:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [info, setInfo] = useState([]);
+
+  async function add() {
+    const responce = await fetch("https://randomuser.me/api/");
+    const data = await responce.json();
+    setInfo(data.results);
+  }
+
+  const information = info.map((props) => {
+    return (
+      <div className="okvir" key={props.id.value}>
+        <div>
+          <img src={props.picture.large}></img>
+        </div>
+        <div>
+          <h2>{props.name.first + " " + props.name.last}</h2>
+        </div>
+        <div className="dole">
+          <div className="info">
+            <p>{"Gender: " + props.gender}</p>
+          </div>
+          <div className="info">
+            <p>{"Age: " + props.dob.age}</p>
+          </div>
+          <div className="info">
+            <p>{"Country: " + props.location.country}</p>
+          </div>
+          <div className="info">
+            <p>{" City: " + props.location.city}</p>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random person generator</h1>
+      {information}
+      <button className="button-77" role="button" onClick={add}>
+        Generate random person
+      </button>
     </div>
   );
 }
